ebf90fccb9c3a2b68e5993e22340199a68051019
Paul <pavel_syrovatsky@mail.ru>
2019-04-24 18:22:21 +0300

    add input form for link and sending to controller

--- src/components/TakeLink.js
+++ src/components/TakeLink.js
@@ -1,15 +1,45 @@
 import React, {Component} from "react";
 import {Link} from 'react-router-dom'
+import axios from 'axios';
 
 class TakeLink extends Component {
 
-    
+
+    constructor(props) {
+        super(props)
+        this.state  = {value:''};
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    handleClick() {
+        axios.get('http://localhost:8080/takeLink/hi')
+        console.log('check');
+    }
+    handleChange(event){
+        this.setState({value: event.target.value});
+    }
+    handleSubmit(){
+        var link = this.state.value;
+        axios.get('http://localhost:8080/takeLink/'+link)
+    }
 
     render() {
         return (
             <div>
-            <h1>In progress..</h1>
+                <h1>In progress..</h1>
                 <Link to='/result'> Test Result</Link>
+                {/*<button className='btn-primary' onClick={this.handleClick}>Send Link</button>*/}
+
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Input gitHub link:
+                        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                    </label>
+                    <input type="submit" value="Send Link"/>
+
+                </form>
 
             </div>
 
